7ebe7a3bfe1dd4fee546872d56414ad25df40c8f
SasaiKudasai <sasha.mittal@yandex.ru>
2023-08-28 22:52:58 +0300

    Исправления Square, создание класса Figure

--- Tetris/Square.cs
+++ Tetris/Square.cs
@@ -4,29 +4,23 @@ namespace Tetris
 {
     public class Square
     {
-        Point[][] points = new Point[2][];
+        Point[] points = new Point[4];
         public Square (int x, int y, char sym) 
         {
-            
-            points[0] = new Point[2];
-            points[1] = new Point[2];
+                       
 
-
-            points[0][0] = new Point(x, y, sym);
-            points[0][1] = new Point(x, y + 1, sym);
-            points[1][1] = new Point(x+1, y+1, sym);
-            points[1][0] = new Point(x+1, y, sym);
+            points[0] = new Point(x, y, sym);
+            points[1] = new Point(x, y + 1, sym);
+            points[2] = new Point(x+1, y+1, sym);
+            points[3] = new Point(x+1, y, sym);
 
         }
 
         public void Draw()
         {
-            for (int i = 0; i < points.Length; i++)
+            foreach (Point p in points)
             {
-                for (int j = 0; j < points[i].Length; j++)
-                {
-                    points[i][j].Draw();
-                }
+                p.Draw();
             }
         }
     }
